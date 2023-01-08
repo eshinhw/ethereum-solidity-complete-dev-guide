13b9302e8e0dfc9af3eab6c2fa09c8cdbabf7359
Eddie Shin <eshinhw@gmail.com>
2023-01-08 15:45:37 +0900

    working on Inbox.test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,3 +4,22 @@ const Web3 = require("web3"); // upper case for constructor
 const web3 = new Web3(ganache.provider());
 
 // Mocha: it, describe, beforeEach
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+beforeEach(() => {const car = new Car();});
+
+describe("Car", () => {
+  it("car can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+  it("car can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
