2f3f87fefb7881a70391dab9323bff7460458df2
Eddie Shin <eshinhw@gmail.com>
2023-01-08 17:08:24 +0900

    working on Inbox.test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,24 +2,38 @@ const assert = require("assert");
 const ganache = require("ganache-cli"); // test local ethereum network
 const Web3 = require("web3"); // upper case for constructor
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
-// Mocha: it, describe, beforeEach
-class Car {
-  park() {
-    return "stopped";
-  }
-  drive() {
-    return "vroom";
-  }
-}
+let accounts;
 
-beforeEach(() => {const car = new Car();});
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+});
 
-describe("Car", () => {
-  it("car can park", () => {
-    assert.equal(car.park(), "stopped");
-  });
-  it("car can drive", () => {
-    assert.equal(car.drive(), "vroom");
+describe("Inbox", () => {
+  it("deploy a contract", () => {
+    console.log(accounts);
   });
 });
+
+// Mocha: it, describe, beforeEach
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
+//   drive() {
+//     return "vroom";
+//   }
+// }
+
+// beforeEach(() => {const car = new Car();});
+
+// describe("Car", () => {
+//   it("car can park", () => {
+//     assert.equal(car.park(), "stopped");
+//   });
+//   it("car can drive", () => {
+//     assert.equal(car.drive(), "vroom");
+//   });
+// });
