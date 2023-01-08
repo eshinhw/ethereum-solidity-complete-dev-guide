0ac2687a0a22972b7f932857dfc628e871701ac7
Eddie Shin <eshinhw@gmail.com>
2023-01-08 09:20:02 +0900

    working on Inbox.test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,6 @@
 const assert = require("assert");
 const ganache = require("ganache-cli"); // test local ethereum network
-const Web3 = require("web3"); // upper case for constructor 
+const Web3 = require("web3"); // upper case for constructor
+const web3 = new Web3(ganache.provider());
+
+// Mocha: it, describe, beforeEach
